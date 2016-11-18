207070247f632e7b32e2003e8e27975aaec2489d
monkey-H <5681713@qq.com>
2016-11-18 02:52:32 +0000

    update

--- ngx_http_upstream_dynamic_hash_module.c
+++ ngx_http_upstream_dynamic_hash_module.c
@@ -1,6 +1,5 @@
-#include <ngx_config.h>
-#include <ngx_core.h>
 #include <ngx_http.h>
+#include <unistd.h>
 
 typedef struct {
     struct sockaddr                *sockaddr;
@@ -19,7 +18,7 @@ typedef struct {
     ngx_uint_t                        number;
     ngx_uint_t                        total_weight;
     unsigned                          weighted:1;
-    ngx_http_upstream_dynamic_hash_peer_t     peer[10];
+    ngx_http_upstream_dynamic_hash_peer_t     peer[91];
 } ngx_http_upstream_dynamic_hash_peers_t;
 
 typedef struct {
@@ -105,11 +104,14 @@ ngx_http_upstream_init_dynamic_hash(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t
     int                             count;
     int                             server_num;
     int*                            entry;
-    ngx_uint_t                      col=7;
+    ngx_uint_t                      col=91;
     ngx_uint_t                      i;
     ngx_http_upstream_dynamic_hash_peers_t *peers;
+    struct timeval start, end;
 
-    fprintf(stderr, "dynamic func %s\n", "init");
+    gettimeofday(&start, NULL);
+
+    //fprintf(stderr, "dynamic func %s\n", "init");
     us->peer.init = ngx_http_upstream_init_dynamic_hash_peer;
 
     server = us->servers->elts;
@@ -142,10 +144,10 @@ ngx_http_upstream_init_dynamic_hash(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t
 
         sprintf(addr_name, "%d", ip_port);
         strcat(addr_name, ip_addr);
-	fprintf(stderr, "addr name %s\n", addr_name);
+	//fprintf(stderr, "addr name %s\n", addr_name);
         server_name[count] = addr_name;
         weight[count] = server[i].weight;
-        fprintf(stderr, "addr weight %d\n", (int)server[i].weight);
+        //fprintf(stderr, "addr weight %d\n", (int)server[i].weight);
         count ++;
 
     }
@@ -157,8 +159,8 @@ ngx_http_upstream_init_dynamic_hash(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t
                                   + sizeof(ngx_http_upstream_dynamic_hash_peer_t) * server_num);
 
     for (i=0; i<col; i++) {
-	ngx_log_stderr(0, "dynamic: %d: name: \"%s\"", i, inet_ntoa(((struct sockaddr_in *)server[entry[i]].addrs[0].sockaddr)->sin_addr));
-	ngx_log_stderr(0, "dynamic: %d: port: \"%d\"", i, (unsigned int)ntohs(((struct sockaddr_in *)server[entry[i]].addrs[0].sockaddr)->sin_port));
+	//ngx_log_stderr(0, "dynamic: %d: name: \"%s\"", i, inet_ntoa(((struct sockaddr_in *)server[entry[i]].addrs[0].sockaddr)->sin_addr));
+	//ngx_log_stderr(0, "dynamic: %d: port: \"%d\"", i, (unsigned int)ntohs(((struct sockaddr_in *)server[entry[i]].addrs[0].sockaddr)->sin_port));
         peers->peer[i].sockaddr = server[entry[i]].addrs[0].sockaddr;
         peers->peer[i].socklen = server[entry[i]].addrs[0].socklen;
         peers->peer[i].name = server[entry[i]].addrs[0].name;
@@ -167,6 +169,10 @@ ngx_http_upstream_init_dynamic_hash(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t
     }
 
     us->peer.data = peers;
+
+    gettimeofday(&end, NULL);
+    fprintf(stderr, "dynamic init end %ld \n", 1000000*end.tv_sec+end.tv_usec);
+    fprintf(stderr, "dynamic init last %ld \n", 1000000*(end.tv_sec-start.tv_sec)+end.tv_usec-start.tv_usec);
 
     return NGX_OK;
 }
@@ -179,10 +185,13 @@ ngx_http_upstream_init_dynamic_hash_peer(ngx_http_request_t *r,
     struct sockaddr_in                     *sin;
     ngx_http_upstream_dynamic_hash_peer_data_t  *iphp;
     ngx_http_upstream_dynamic_hash_conf_t	 *uhcf;
+    struct timeval start;
 
     ngx_str_t val;
 
-    fprintf(stderr, "dynamic func %s\n", "init peer");
+    gettimeofday(&start, NULL);
+
+    fprintf(stderr, "dynamic init hash peer %ld\n", 1000000*start.tv_sec + start.tv_usec);
 
     uhcf = ngx_http_conf_upstream_srv_conf(us, ngx_http_upstream_dynamic_hash_module);
 
@@ -207,15 +216,15 @@ ngx_http_upstream_init_dynamic_hash_peer(ngx_http_request_t *r,
     sin = (struct sockaddr_in *) r->connection->sockaddr;
     strcat(name, inet_ntoa(sin->sin_addr));
 
-    iphp->hash = h1((char *)val.data, val.len) % 7;
+    iphp->hash = h1((char *)val.data, val.len) % 91;
 
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "dynamic client name %s", name
-                   );
+    //ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+    //               "dynamic client name %s", name
+    //               );
     
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "dynamic iphp hash %d",
-                   iphp->hash);
+    //ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+    //               "dynamic iphp hash %d",
+    //               iphp->hash);
 
     iphp->tries = 0;
     //iphp->get_rr_peer = ngx_http_upstream_get_round_robin_peer;
@@ -231,19 +240,22 @@ ngx_http_upstream_get_dynamic_hash_peer(ngx_peer_connection_t *pc, void *data)
 
     int                    hash;
     ngx_http_upstream_dynamic_hash_peer_t  *peer;
+    struct timeval start, end;
 
-    fprintf(stderr, "dynamic func %s\n", "get peer");
+    gettimeofday(&start, NULL);
+
+    //fprintf(stderr, "dynamic func %s\n", "get peer");
 
     pc->cached = 0;
     pc->connection = NULL;
 
-    fprintf(stderr, "dynamic func2 %s\n", "get peer");
+    //fprintf(stderr, "dynamic func2 %s\n", "get peer");
     hash = iphp->hash;
-    fprintf(stderr, "dynamic func3 %d\n", hash);
+    //fprintf(stderr, "dynamic func3 %d\n", hash);
 
     peer = &iphp->peers->peer[hash];
 
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "dynamic sockaddr: %s", "world");
+    //ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "dynamic sockaddr: %s", "world");
 
     pc->sockaddr = peer->sockaddr;
     pc->socklen = peer->socklen;
@@ -251,15 +263,19 @@ ngx_http_upstream_get_dynamic_hash_peer(ngx_peer_connection_t *pc, void *data)
 
     print_sockaddr(pc->log, peer->sockaddr);
 
+    gettimeofday(&end, NULL);
+    fprintf(stderr, "dynamic get end %ld\n", 1000000*end.tv_sec + end.tv_usec);
+    fprintf(stderr, "dynamic get last %ld\n", 1000000*(end.tv_sec-start.tv_sec) + end.tv_usec - start.tv_usec);
+
     return NGX_OK;
 }
 
 static void print_sockaddr(ngx_log_t *log, struct sockaddr *addr) {
-    struct sockaddr_in *ip;
+    //struct sockaddr_in *ip;
 
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "dynamic sockaddr: %s", "world");
-    ip = (struct sockaddr_in *)addr;
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "dynamic sockaddr: %s", inet_ntoa(ip->sin_addr));
+    //ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "dynamic sockaddr: %s", "world");
+    //ip = (struct sockaddr_in *)addr;
+    //ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "dynamic sockaddr: %s", inet_ntoa(ip->sin_addr));
 }
 
 static char *
@@ -275,7 +291,7 @@ ngx_http_upstream_dynamic_hash(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     uscf = ngx_http_conf_get_module_srv_conf(cf, ngx_http_upstream_module);
     uhcf = ngx_http_conf_upstream_srv_conf(uscf, ngx_http_upstream_dynamic_hash_module);
 
-    fprintf(stderr, "dynamic func1 %s\n", "hash");
+    //fprintf(stderr, "dynamic func1 %s\n", "hash");
 
     ngx_memzero(&sc, sizeof(ngx_http_script_compile_t));
 
@@ -286,13 +302,12 @@ ngx_http_upstream_dynamic_hash(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     sc.complete_lengths = 1;
     sc.complete_values = 1;
 
-    fprintf(stderr, "dynamic func2 %s\n", "hash");
+    //fprintf(stderr, "dynamic func2 %s\n", "hash");
     if (ngx_http_script_compile(&sc) != NGX_OK) {
-    	fprintf(stderr, "dynamic func2.5 %s\n", "hash");
 	return NGX_CONF_ERROR;
     }
 
-    fprintf(stderr, "dynamic func2 %s\n", "hash");
+    //fprintf(stderr, "dynamic func2 %s\n", "hash");
     
     uscf->peer.init_upstream = ngx_http_upstream_init_dynamic_hash;
 
@@ -302,7 +317,7 @@ ngx_http_upstream_dynamic_hash(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
                   |NGX_HTTP_UPSTREAM_DOWN
 		  |NGX_HTTP_UPSTREAM_WEIGHT;
 
-    fprintf(stderr, "dynamic func3 %s\n", "hash");
+    //fprintf(stderr, "dynamic func3 %s\n", "hash");
 
     return NGX_CONF_OK;
 }
